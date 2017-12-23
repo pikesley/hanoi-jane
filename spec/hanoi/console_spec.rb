eb42cfd43c2d7768d8e666b84002acd2e01cba9b
pikesley <github@orgraphone.org>
2017-12-23 17:19:43 +0000

    Move this into its own Formatter
    Is this a formatter? Cannot remember enough GoF

--- spec/hanoi/console_spec.rb
+++ spec/hanoi/console_spec.rb
@@ -16,46 +16,46 @@ module Hanoi
         towers.move
         expect(towers.console).to eq "                       \n                       \n ooooo     o      ooo  \n"
       end
+    end
 
-      context 'rotates the stacks' do
-        it 'rotates the initial state' do
-          expect(Towers.rotate [[2, 1, 0], [nil, nil, nil], [nil, nil, nil]]).to eq [
-            [0, nil, nil],
-            [1, nil, nil],
-            [2, nil, nil]
-          ]
-        end
+    describe Console do
+      it 'rotates the initial state' do
+        expect(Console.rotate [[2, 1, 0], [nil, nil, nil], [nil, nil, nil]]).to eq [
+          [0, nil, nil],
+          [1, nil, nil],
+          [2, nil, nil]
+        ]
+      end
 
-        it 'rotates the first state' do
-          expect(Towers.rotate [[2, 1, nil], [0, nil, nil], [nil, nil, nil]]).to eq [
-            [nil, nil, nil],
-            [1, nil, nil],
-            [2, 0, nil]
-          ]
-        end
+      it 'rotates the first state' do
+        expect(Console.rotate [[2, 1, nil], [0, nil, nil], [nil, nil, nil]]).to eq [
+          [nil, nil, nil],
+          [1, nil, nil],
+          [2, 0, nil]
+        ]
+      end
 
-        it 'rotates the second state' do
-          expect(Towers.rotate [[2, nil, nil], [0, nil, nil], [1, nil, nil]]).to eq [
-            [nil, nil, nil],
-            [nil, nil, nil],
-            [2, 0, 1]
-          ]
-        end
+      it 'rotates the second state' do
+        expect(Console.rotate [[2, nil, nil], [0, nil, nil], [1, nil, nil]]).to eq [
+          [nil, nil, nil],
+          [nil, nil, nil],
+          [2, 0, 1]
+        ]
       end
 
       it 'pads an array' do
-        expect(Towers.pad [0], 3).to eq [nil, nil, 0]
+        expect(Console.pad [0], 3).to eq [nil, nil, 0]
       end
 
       it 'makes a disc' do
-        expect(Towers.make_disc 0, 5).to eq '  o  '
-        expect(Towers.make_disc nil, 5). to eq '     '
+        expect(Console.make_disc 0, 5).to eq '  o  '
+        expect(Console.make_disc nil, 5). to eq '     '
       end
 
       it 'scales a size' do
-        expect(Towers.scale 0).to eq 1
-        expect(Towers.scale 1).to eq 3
-        expect(Towers.scale 2).to eq 5
+        expect(Console.scale 0).to eq 1
+        expect(Console.scale 1).to eq 3
+        expect(Console.scale 2).to eq 5
       end
     end
   end
