7fe88e988043eb0ec6385c07ae34faf105638362
pikesley <sam.pikesley@gmail.com>
2018-09-23 19:05:06 +0100

    Placeholder

---
+++ spec/hanoi/jane/serialise_spec.rb
@@ -0,0 +1,11 @@
+module Hanoi
+  module Jane
+    describe RegularTowers do
+      context 'serialise state' do
+        it 'serialises itself' do
+          expect(true).to be false
+        end
+      end
+    end
+  end
+end
